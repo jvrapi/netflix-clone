405243599d173bd28e166cf4942034d713de2e1b
João Vitor Santos <joaooviitorr@hotmail.com>
2021-09-13 20:35:52 -0300

    Structure of featured component created

--- src/Components/FeaturedMovie/index.tsx
+++ src/Components/FeaturedMovie/index.tsx
@@ -1,13 +1,49 @@
 import React from 'react'
 import { TmdbDataItemResult } from 'types/Tmdb'
+import './styles.css'
 
 type Props = {
   item: TmdbDataItemResult
 }
 
 const FeaturedMovie: React.FC<Props> = ({ item }) => {
-  console.log(item)
-  return <div>FeaturedMovie</div>
+  const firstDate = new Date(item.first_air_date)
+  const genres: string[] = []
+
+  item.genres?.forEach(({ name }) => genres.push(name))
+
+  return (
+    <section
+      className="featured"
+      style={{
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+      }}
+    >
+      <div className="featured--vertical">
+        <div className="featured--horizontal">
+          <div className="featured--name">{item.original_name}</div>
+          <div className="featured--info">
+            <div className="featured--points">{item.vote_average} pontos</div>
+            <div className="featured--year">{firstDate.getFullYear()}</div>
+            <div className="featured--season">
+              {item.number_of_seasons} temporada
+              {item.number_of_seasons !== 1 && 's'}
+            </div>
+            <div className="featured--description">{item.overview}</div>
+            <div className="featured--butons">
+              <a href={`/watch/${item.id}`}>► Assistir</a>
+              <a href={`/list/add/${item.id}`}>+ Minha Lista</a>
+            </div>
+            <div className="featured--genres">
+              <strong>Gêneros: {genres.join(', ')}</strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export { FeaturedMovie }
--- src/types/Tmdb.ts
+++ src/types/Tmdb.ts
@@ -1,5 +1,10 @@
-export type TmdbDataItemResult = {
-  backdrop_path: string
+type Genre = {
+  id: number
+  name: string
+}
+// Dados recebidos quando faz a request para obter a lista de filmes
+export type TmdbDataItemResultBase = {
+  backdrop_path?: string
   first_air_date: string
   genre_ids: number[]
   id: number
@@ -12,6 +17,12 @@ export type TmdbDataItemResult = {
   poster_path: string
   vote_average: number
   vote_count: number
+}
+
+// Dados recebidos quando faz a request dos detalhes
+export type TmdbDataItemResult = TmdbDataItemResultBase & {
+  number_of_seasons?: number
+  genres?: Genre[]
 }
 
 export type TmdbDataItem = {

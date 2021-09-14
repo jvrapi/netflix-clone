a1b06a2780755db33cce7414a2177e9467d07118
João Vitor Santos <joaooviitorr@hotmail.com>
2021-09-14 19:27:59 -0300

    Horizontal scroll

--- src/Components/ListMovies/index.tsx
+++ src/Components/ListMovies/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore'
+import NavigateNextIcon from '@material-ui/icons/NavigateNext'
 import { TmdbDataItem } from 'types/Tmdb'
 import './styles.css'
 
@@ -8,11 +10,43 @@ type Props = {
 }
 
 const ListMovies: React.FC<Props> = ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(-400)
+
+  const handleLeftArrow = () => {
+    // empurra a lista para a direita
+    let newScrollValue = scrollX + Math.round(window.innerWidth / 2)
+    if (newScrollValue > 0) {
+      newScrollValue = 0
+    }
+
+    setScrollX(newScrollValue)
+  }
+
+  const handleRightArrow = () => {
+    // empurra a lista para a esquerda
+    let newScrollValue = scrollX - Math.round(window.innerWidth / 2)
+
+    const listWidth = items.results.length * 150
+
+    if (window.innerWidth - listWidth > newScrollValue) {
+      newScrollValue = window.innerWidth - listWidth - 60 // tira os 30 de padding da lista
+    }
+    setScrollX(newScrollValue)
+  }
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+      <div className="movieRow--left" onClick={handleLeftArrow}>
+        <NavigateBeforeIcon style={{ fontSize: 50 }} />
+      </div>
+      <div className="movieRow--right" onClick={handleRightArrow}>
+        <NavigateNextIcon style={{ fontSize: 50 }} />
+      </div>
       <div className="movieRow--listarea">
-        <div className="movieRow--list">
+        <div
+          className="movieRow--list"
+          style={{ marginLeft: scrollX, width: items.results.length * 150 }}
+        >
           {items.results.length > 0 &&
             items.results.map((item, key) => (
               <div
